fc2189f28bd5b5193c8f5fe718acb06d76f398db
joel-razor1 <joelabraham8857@gmail.com>
2019-10-12 20:36:25 +0530

    Completed till strings

---
+++ demo.js
@@ -0,0 +1,66 @@
+//This is how you write a single line comment.
+/*
+This is how
+you write 
+a multiline
+comment
+*/
+//Data Types and Variables
+var number1 = 12; //Btw you can comment like this too.
+var number2 = 12.1;
+// console.log(number1, number2);
+var string1 = "Joel Johnson";
+// console.log(string1);
+var p;
+// console.log(p);
+var sathyaVachan = true;
+var kallathram = false;
+// console.log(sachBolo);
+// console.log(kallathram);
+var khokla = null;
+// console.log(khokla);
+var obj = {};
+// console.log(typeof obj);
+const symbol1 = Symbol("description"); //simply Google it what it is. Even I didn't know it till now.
+// console.log(typeof symbol1);
+
+//storing values with Assignment Operator
+var a = 101010;
+// console.log(a);
+//Variable is case Sensitive. uNdErStOoD?
+var DOG = "I am a huge and cute dog.";
+var dog = "I am a just a cute doggo.";
+// console.log(DOG);
+// console.log(dog);
+
+//BASIC MATH OPERATION
+// console.log(number1 + number2);
+// console.log(number1 - number2);
+// console.log(number1 * number2);
+// console.log(number1 / number2);
+// console.log(number1 % number2);
+number1++;
+// console.log(number1);
+number1--;
+// console.log(number1);
+
+//COMPOUND ASSIGNMENT
+number1 += 13;
+// console.log(number1);
+number1 -= 13;
+// console.log(number1);
+number1 *= 13;
+// console.log(number1);
+number1 /= 13;
+// console.log(number1);
+number1 %= 13;
+// console.log(number1);
+
+//ESCAPE SEQUENCE
+var text1 = 'And the teacher said "It\'s too Easy".';
+// console.log(text1);
+var text1 = 'You know what this "\\" is?';
+// console.log(text1);
+var text2 = "I will study tonight.";
+var text3 = " I am Joking.";
+// console.log(text2 + text3);
